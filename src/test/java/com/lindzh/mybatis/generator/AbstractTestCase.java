9549f6099bd1c7a855d9e800c061595555a9f0ee
鲁般 <dezhi.ldz@alibaba-inc.com>
2018-04-14 21:40:18 +0800

    add h2 test case

--- src/test/java/com/lindzh/mybatis/generator/AbstractTestCase.java
+++ src/test/java/com/lindzh/mybatis/generator/AbstractTestCase.java
@@ -1,12 +1,19 @@
 package com.lindzh.mybatis.generator;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.h2.tools.Server;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 
 /**
  * 
@@ -14,11 +21,29 @@ import org.junit.Before;
  * 2015年11月15日 上午11:14:28
  */
 public abstract class AbstractTestCase {
-	
+
+	private static Server h2Server;
+
+	@BeforeClass
+	public static void createH2Server() throws SQLException, ClassNotFoundException {
+		String[] args = new String[]{};
+		h2Server = Server.createTcpServer(args).start();
+		Class.forName("org.h2.Driver");
+		String path = new File("src/test/resources/test_table.sql").getAbsolutePath();
+		String url = "jdbc:h2:mem:test;INIT=RUNSCRIPT FROM '"+path+"'";
+//		String url = "jdbc:h2:mem:test";
+		try{
+			Connection connection = DriverManager.getConnection(url, "sa", "");
+			connection.setAutoCommit(true);
+		}catch (Exception e){
+
+		}
+	}
+
 	protected SqlSessionFactory factory;
 
 	@Before
-	public void init() {
+	public void init() throws SQLException, ClassNotFoundException {
 		String resource = "mybatis-config.xml";
 		Reader reader;
 		try {
@@ -30,4 +55,10 @@ public abstract class AbstractTestCase {
 		factory = new SqlSessionFactoryBuilder().build(reader);
 	}
 
+	@AfterClass
+	public static void stopH2Server(){
+		if(h2Server!=null){
+			h2Server.stop();
+		}
+	}
 }
--- src/test/java/com/lindzh/mybatis/generator/StuCourseDaoTest.java
+++ src/test/java/com/lindzh/mybatis/generator/StuCourseDaoTest.java
@@ -9,11 +9,13 @@ import com.lindzh.mybatis.generator.bean.StuCourse;
 import com.lindzh.mybatis.generator.dao.StuCourseDao;
 
 public class StuCourseDaoTest extends AbstractTestCase{
-	
+
+
+	@Test
 	public void addCourse(){
 		StuCourseDao courseDao = factory.openSession(true).getMapper(StuCourseDao.class);
 		Random random = new Random();
-		for(int i=0;i<1000;i++){
+		for(int i=0;i<10;i++){
 			StuCourse course = new StuCourse();
 			course.setAddTime(System.currentTimeMillis());
 			long courseId = 1005;//random.nextInt(10000)+1000;
@@ -25,10 +27,20 @@ public class StuCourseDaoTest extends AbstractTestCase{
 			courseDao.addStuCourse(course);
 		}
 	}
-	
+
+	@Test
 	public void testId(){
-		long id = 1;
+		long id = 100;
 		StuCourseDao courseDao = factory.openSession(true).getMapper(StuCourseDao.class);
+		StuCourse course1 = new StuCourse();
+		course1.setAddTime(System.currentTimeMillis());
+		long courseId = 1005;
+		course1.setCourseId(courseId);
+		course1.setStuId(10098);
+		course1.setScore(88);
+		courseDao.addStuCourse(course1);
+		id = course1.getId();
+
 		StuCourse course = courseDao.getById(id);
 		System.out.println("id get:"+JSONUtils.toJSON(course));
 		course.setScore(100);
@@ -37,13 +49,22 @@ public class StuCourseDaoTest extends AbstractTestCase{
 		System.out.println("update get:"+JSONUtils.toJSON(course));
 		courseDao.deleteById(id);
 		course = courseDao.getById(id);
-		System.out.println("delete get:"+JSONUtils.toJSON(course));
+		System.out.println("delete get:"+course);
 	}
-	
+
+	@Test
 	public void testUnique(){
 		long userId=49732;
 		long courseId=10388;
 		StuCourseDao courseDao = factory.openSession(true).getMapper(StuCourseDao.class);
+
+		StuCourse course1 = new StuCourse();
+		course1.setAddTime(System.currentTimeMillis());
+		course1.setCourseId(courseId);
+		course1.setStuId(userId);
+		course1.setScore(88);
+		courseDao.addStuCourse(course1);
+
 		StuCourse course = courseDao.getByUserAndCourse(userId, courseId);
 		System.out.println("stu_course get:"+JSONUtils.toJSON(course));
 		
@@ -62,6 +83,19 @@ public class StuCourseDaoTest extends AbstractTestCase{
 	public void testLimitOffset(){
 		long courseId=1005;
 		StuCourseDao courseDao = factory.openSession(true).getMapper(StuCourseDao.class);
+
+		Random random = new Random();
+		for(int i=0;i<10;i++){
+			StuCourse course = new StuCourse();
+			course.setAddTime(System.currentTimeMillis()-random.nextInt(100000));
+			long stuid = random.nextInt(50000)+1000;
+			course.setCourseId(courseId);
+			course.setStuId(stuid);
+			int score = random.nextInt(50)+50;
+			course.setScore(score);
+			courseDao.addStuCourse(course);
+		}
+
 		long count = courseDao.getCountByCourseAndTime(courseId, 0, System.currentTimeMillis());
 		System.out.println("count:"+count);
 		List<StuCourse> courses = courseDao.getListByCourseAndTime(courseId, 0, System.currentTimeMillis(), 10, 0);
